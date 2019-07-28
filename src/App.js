6ae4925938d423ff3f7e8953f254963efcdf3c12
jetsetPanda <marionong@gmail.com>
2019-07-28 00:56:20 -0400

    setup npm libs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { render } from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Drilldown from 'react-router-drilldown'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+ return (
+  <Router>
+    <Drilldown>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/people" component={People} />
+      <Route exact path="/films" component={Films} />
+      <Route exact path="/starships" component={Starships} />
+      <Route exact path="/species" component={Species} />
+      <Route exact path="/planets" component={Planets} />
+    </Drilldown>
+  </Router>
+  
   );
+
 }
 
 export default App;
