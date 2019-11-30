8c2fab1d7ba81776f5f767933f9a8cc8469abb57
jetsetPanda <marionong@gmail.com>
2019-11-30 16:14:59 -0500

    init category views

---
+++ src/views/Category.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Circlemenu from '../components/Circlemenu';
+import Spinner from 'react-bootstrap/Spinner';
+import Navicategory from '../components/Navicategory';
+import Synopsys from '../components/Synopsys';
+import Quote from '../components/Quote';
+
+export default function Films(props) {
+    // init state
+    const initialState = {
+        dataset: [],
+        isLoading: true,
+    }
+
+    // user state getter and setter via useState()
+    const [dataset, setDataset] = useState(initialState);
+
+    // useEffect method to get SWAPI data (my alternative to LM componentDidMount)
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios(`https://swapi.co/api/films/`);
+
+            setDataset(data.results);// results array to films: {} above
+            console.log(data.results);
+        }
+        //asynchronious function is invoked by:
+        fetchData();
+    }, []) // "[]" prevents useEffect from executing infinite loop
+
+    return dataset.isLoading? (
+        <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+        </Spinner>
+    ) : (
+        <div className="container">
+
+            <Navicategory brand={'FILMS'}/>
+            <Quote />
+            {dataset.map((film, index) => (
+
+
+                <Link to={{ pathname: '/films/:id', state: { url : film.url }}} style={{textDecoration: 'none'}} >
+                    <div className="card mb-1">
+                        <div className="card-body ">
+                            <div className="d-inline">
+                                <Circlemenu text={film.title}/>
+                            </div>
+                            <div className="d-inline">
+                                <h3 className="card-title menufont">{film.title}</h3>
+                                <p className="card-subtitle mb-3 menufont">
+                                    <Synopsys crawl={film.opening_crawl}/>...<br/>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
+}
